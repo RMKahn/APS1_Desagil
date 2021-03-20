6994d2d7dcf566b0d4fa0c38c94570e3b67e0f15
RMKahn <rafaelmk@al.insper.edu.br>
2021-03-20 14:16:57 -0300

    Classe pedido finalziada

--- APS1/src/br/edu/insper/desagil/vaimalte/Pedido.java
+++ APS1/src/br/edu/insper/desagil/vaimalte/Pedido.java
@@ -2,8 +2,28 @@ package br.edu.insper.desagil.vaimalte;
 
 public class Pedido {
 	private Produto produto;
-	private int quantidade;
+	private int quantidade = 1;
 	
-	
+	public Pedido(Produto produto) {
+		super();
+		this.produto = produto;
+	}
 
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public int incrementa(int quantidade){
+		quantidade += 1;
+		return quantidade;
+	}
+
+	public float calcula(int quantidade, Produto produto) {
+		float total = quantidade * produto.getPreco();
+		return total;
+	}
 }
