8500608f1530104d5046c273c2b2113e1832b14e
RMKahn <rafaelmk@al.insper.edu.br>
2021-03-21 16:14:51 -0300

    Classe carrinho finalizada

--- APS1/src/br/edu/insper/desagil/vaimalte/Carrinho.java
+++ APS1/src/br/edu/insper/desagil/vaimalte/Carrinho.java
@@ -3,6 +3,28 @@ package br.edu.insper.desagil.vaimalte;
 import java.util.List;
 
 public class Carrinho {
-	private List<Pedido> pedidos;
+	private List<Pedido> carrinho;
+	private Pedido pedido;
+
+	public Carrinho() {
+		super();
+	}
+
+	public List<Pedido> getPedidos() {
+		return carrinho;
+	}
+	
+	public List<Pedido> adiciona(Produto produto) {
+		for (Pedido existente : carrinho) {
+			if (existente.getProduto().equals(produto)) {
+				pedido.incrementa(pedido.getQuantidade());
+			}
+		}
+		pedido = new Pedido(produto);
+		carrinho.add(pedido);
+		
+		return carrinho;
+		
+	}
 
 }
